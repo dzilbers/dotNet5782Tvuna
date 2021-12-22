ba85f26bfa7cab75593214971eba1004b910fb79
Dan Zilberstein <dzilbers@gmail.com>
2021-12-22 16:12:44 +0200

    BINDING

---
+++ WpfBinding/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+using System.Windows.Media;
+
+namespace WpfBinding
+{
+    //Drone drone = new Drone();
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Page
+    {
+        public static string MyName { get; set; } = "Next1";
+
+        public MainWindow() => InitializeComponent();
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var brush = (LinearGradientBrush)this.Resources["myLinGradBrush"];
+            brush.GradientStops[0].Color = Colors.Green;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Resources["myLinGradBrush"] = new SolidColorBrush(Colors.GreenYellow);
+        }
+
+        private void btnNext2_Click(object sender, RoutedEventArgs e)
+        {
+            string str = (String)btnNext2.Content;
+            btnNext2.Content = str.Length > 0 ? str.Substring(1) : "I am still here!";
+        }
+    }
+}
