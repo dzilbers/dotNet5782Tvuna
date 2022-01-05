9630397dfb7ed6f5fcb133bae80a06cf218716ea
Dan Zilberstein <dzilbers@gmail.com>
2022-01-05 15:35:43 +0200

    attributes

---
+++ Attributes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Attributes
+{
+    [DebuggerDisplay("{Name,nq}({Id,h})")]
+    class Person
+    {
+        internal int Id;
+        internal string Name;
+        [Obsolete("Please stop using this function, use f2(int) instead", false)]
+        internal void f1() { }
+        internal void f2(int num) { }
+        public override string ToString() => $"Person: #{Id}, name: {Name}";
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person { Id = 123, Name = "Shira" };
+            p.f1();
+            Console.WriteLine(p);
+        }
+    }
+}
